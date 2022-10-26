0764587c4899bd13fe3ac5317fccb9ec869f9c24
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-26 19:51:35 -0300

    Adicionando validação de contato + zoom do mapa

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,69 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOK=false
+let emailOK=false
+let assuntoOK=false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width='100%'
+email.style.width='100%'
+
+function validaNome(){
+    let txtNome = document.querySelector("#txtNome")
+    if (nome.value.lenght<=2){
+        txtNome.innerHTML='Nome inválido!'
+        txtNome.style.color='red'
+    }else{
+        txtNome.innerHTML='Nome válido'
+        txtNome.style.cor='green'
+        nomeOK=true
+    }
+}
+
+function validaEmail(){
+    let txtEmail=document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.co')==-1 || email.value.indexOf('.org')==-1){
+        txtEmail.innerHTML='E-mail inválido'
+        txtEmail.style.color='red'
+        email.style.backgroundColor='red'
+    }else{
+        txtEmail.innerHTML='E-mail válido'
+        txtEmail.style.color='green'
+        emailOK=true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto=document.querySelector('#txtAssunto')
+    if(assunto.value.lenght>=100){
+        txtAssunto.innerHTML= 'Texto excedeu o limite de 100 caracteres.'
+        txtAssunto.style.color='red'
+        txtAssunto.style.display='block'
+    }else{
+        txtAssunto.style.display='none'
+        assuntoOK=true
+    }
+}
+
 const botao = document.querySelector("#enviar")
 console.log(botao)
 
+
 function envio(){
-    alert('Mensagem enviada com sucesso!')
-    botao.innerHTML='Enviado!'
+    if(nomeOK==true && emailOK==true && assuntoOK ==true){
+        alert('Mensagem enviada com sucesso!')
+    }else{
+    alert('Preencha o formulário corretamente antes de enviar.')
+    }
 }
 
-const nome = document.querySelector('#nome')
+function mapaZoom(){
+    mapa.style.width = '800 px'
+    mapa.style.height = '600px'
+}
 
-function validaNome(){
-    console.log(nome.value.lenght)
-    if(nome.value.lenght<=2){
-        alert('Nome inválido.')
-    }
-    
+function mapaNormal(){
+    mapa.style.width = '400 px'
+    mapa.style.height = '250px'
 }
