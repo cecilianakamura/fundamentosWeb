e9a72d05a052df8eba85719f6f160f2eb624d6fa
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-26 10:11:30 -0300

    Adicionando funcionalidade no botão de Contato

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,17 @@
-console.log("Olá!")
+const botao = document.querySelector("#enviar")
+console.log(botao)
+
+function envio(){
+    alert('Mensagem enviada com sucesso!')
+    botao.innerHTML='Enviado!'
+}
+
+const nome = document.querySelector('#nome')
+
+function validaNome(){
+    console.log(nome.value.lenght)
+    if(nome.value.lenght<=2){
+        alert('Nome inválido.')
+    }
+    
+}
